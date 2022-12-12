0850f1d3f17c7cad0ff5d8cf1d3573d1dffc8a82
TerezaSimkova <tereza.simkova251@gmail.com>
2022-12-12 23:05:32 +0100

    feat: add stars rating

---
+++ GoodReadsProject/ClientApp/src/components/EditPopUp.js
@@ -0,0 +1,14 @@
+﻿import React, { Component } from 'react';
+
+export class Info extends Component {
+    render() {
+        return (
+            <div className="dark_background">
+                <div className="dark_container_content">
+                    <div className="dark_inner_box">
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
--- GoodReadsProject/ClientApp/src/components/MyBookList.js
+++ GoodReadsProject/ClientApp/src/components/MyBookList.js
@@ -2,6 +2,13 @@
 import { SideNav } from './SideNav';
 import { User } from './User';
 import { Breadcrumbs } from './Breadcrumbs';
+import StarRatingComponent from 'react-star-rating-component';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import 'font-awesome/css/font-awesome.min.css';
+import {
+    faPencil,
+    faTrash
+} from '@fortawesome/free-solid-svg-icons'
 
 export class BookList extends Component {
     constructor(props) {
@@ -10,6 +17,7 @@ export class BookList extends Component {
         this.state = {
             error: null,
             isLoaded: false,
+            rating: 1,
             Books: [],
         };
     }
@@ -20,7 +28,7 @@ export class BookList extends Component {
                 (result) => {
                     this.setState({
                         isLoaded: true,
-                        Books: result
+                        Books: result,
                     });
                 },
                 (error) => {
@@ -31,8 +39,11 @@ export class BookList extends Component {
                 }
             )
     }
+    onStarClick(nextValue, prevValue, name) {
+        this.setState({ rating: nextValue });
+    }
     render() {
-        const { error, isLoaded, Books } = this.state;
+        const { error, isLoaded, Books, rating } = this.state;
         if (error) {
             return <div>Error: {error.message}</div>;
         } else if (!isLoaded) {
@@ -45,20 +56,35 @@ export class BookList extends Component {
                     <User />
                     <div className="container_content">
                         <div className="books_row">
-                            <ul>
+                            <div>
                                 {Books.map(book => (
-                                    <li key={book.bookId}>
+                                    <div key={book.bookId}>
                                         <div className="book_content">
-                                            <h3>{book.bookName}</h3>
-                                            <h4>{book.author}</h4>
-                                            <p>{book.bookDescription}</p>
-                                            <p className="book_rating">{book.ratings[0].countStars}</p>
-                                            <p><b>{book.ratings[0].title}</b></p>
-                                            <p>{book.ratings[0].description}</p>
+                                            <div className="book_info">
+                                                <h3>{book.bookName}</h3>
+                                                <h4>{book.author}</h4>
+                                                <p>{book.bookDescription}</p>
+                                            </div>
+                                            <div className="book_stars">
+                                                <div className="book_stars_component">
+                                                    <StarRatingComponent
+                                                        key={book.ratings[0].RatingId}
+                                                        name="rate1"
+                                                        starCount={5}
+                                                        value={book.ratings[0].countStars}
+                                                    />
+                                                    <div className="book_stars_icons">
+                                                        <FontAwesomeIcon icon={faPencil} size="sm" />
+                                                        <FontAwesomeIcon icon={faTrash} size="sm" />
+                                                    </div>
+                                                </div>
+                                                <p><b>{book.ratings[0].title}</b></p>
+                                                <p>{book.ratings[0].description}</p>
+                                            </div>
                                         </div>
-                                    </li>
+                                    </div>
                                 ))}
-                            </ul>
+                            </div>
                         </div>
                     </div>
                 </div>
